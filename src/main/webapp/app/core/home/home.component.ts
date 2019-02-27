7e9e97d8d8ca6b2b3ab7c93f56006f5a7209d785
Sahbi KTIFA <sahbi.ktifa@gmail.com>
2019-02-27 14:58:00 +0100

    NASA Show

--- src/main/webapp/app/core/home/home.component.ts
+++ src/main/webapp/app/core/home/home.component.ts
@@ -1,11 +1,25 @@
 import Component from 'vue-class-component';
 import { Inject, Vue } from 'vue-property-decorator';
 import LoginService from '@/account/login.service';
+import L from 'leaflet';
+import Axios from 'axios';
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 
-@Component
+@Component({
+  components: { LMap, LTileLayer, LMarker }
+})
 export default class Home extends Vue {
   @Inject('loginService')
   private loginService: () => LoginService;
+  public marker = new L.LatLng(0, 0);
+
+  created() {
+    setInterval(() => {
+      Axios.get('https://api.wheretheiss.at/v1/satellites/25544').then(res => {
+        this.marker = new L.LatLng(res.data.latitude, res.data.longitude);
+      });
+    }, 2000);
+  }
 
   public openLogin(): void {
     this.loginService().openLogin((<any>this).$root);
